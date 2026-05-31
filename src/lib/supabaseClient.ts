52ed0ad5297562d5690d4e2d256ec788d0128d13
refugiocampestreaguazul-commits <refugiocampestreaguazul@gmail.com>
2026-05-31 16:00:44 -0500

    test build

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://placeholder.supabase.co";
+  process.env.NEXT_PUBLIC_SUPABASE_URL ??
+  "https://dummy.supabase.co";
 
 const supabaseAnonKey =
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "placeholder-key";
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ??
+  "dummy-key";
 
 export const supabase = createClient(
   supabaseUrl,
